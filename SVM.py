5a272e69533daa1597e85e426a424bed4eb55ce4
Elias <MagicElyas@users.noreply.github.com>
2020-01-11 17:01:52 +0100

    Adding the necessary code to make the experiment work - Added loop for executing 30 times - Added dataframe creation and fulfillment - Added elapsed time measurement - Added basic structure of the csv

--- SVM.py
+++ SVM.py
@@ -3,10 +3,17 @@ from sklearn import svm, metrics
 from sklearn.model_selection import train_test_split
 from keras.datasets import mnist
 from sklearn.metrics import accuracy_score, mean_squared_error
+import pandas as pd
+import random
+import time
+
+try:
+  df = pd.read_csv("stats/svm.csv")
+except FileNotFoundError as e:
+  df = pd.DataFrame(columns=['execno', 'acc', 'mse', 'exec_time'])
+  df.to_csv("stats/svm.csv")
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-
 # To apply a classifier on this data, we need to flatten the image, to
 # turn the data in a (samples, feature) matrix:
 n_samples = x_train.shape[0]
@@ -16,17 +23,21 @@ x_test = x_test.reshape((n_samples_test, -1))
 print(x_train.shape)
 print(x_test.shape)
 
-# Create a classifier: a support vector classifier
-classifier = svm.SVC(gamma=0.001, verbose=True, kernel='rbf')
+for i in range(30):
+  df = pd.read_csv("stats/svm.csv")
+  start = time.time()
+  # Create a classifier: a support vector classifier
+  classifier = svm.SVC(gamma=0.001, verbose=True, kernel='rbf',
+                      random_state= random.randint(0,101))
 
-# We learn the digits on the first half of the digits
-classifier.fit(x_train, y_train)
+  classifier.fit(x_train, y_train)
+  predicted = classifier.predict(x_test)
 
-# Now predict the value of the digit on the second half:
-predicted = classifier.predict(x_test)
-
-acc = accuracy_score(y_test, predicted)
-mse = mean_squared_error(y_test, predicted)
-
-print("Accuracy: "+str(acc))
-print("MSE: "+str(mse))
+  acc = accuracy_score(y_test, predicted)
+  mse = mean_squared_error(y_test, predicted)
+  print("Accuracy: "+str(acc))
+  print("MSE: "+str(mse))
+  end = time.time()
+  df.append({'execno': i, 'acc': acc, 'mse': mse, 'exec_time': end-start},
+            ignore_index=True)
+  df.to_csv("stats/svm.csv")
